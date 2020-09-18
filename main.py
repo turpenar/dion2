ab13844806e63009fdaa7e403a873378035b193a
unknown <aturpening@gmail.com>
2020-09-17 23:57:15 -0700

    started making the game window. needs a lot of work.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -21,7 +22,8 @@ class Todo(db.Model):
 def index():
     
     if request.method == "POST":
-        return "hello"
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
         
         try:
             db.session.add(new_task)
@@ -31,7 +33,8 @@ def index():
             return "There was an issue adding your item"
         
     else:
-        return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
